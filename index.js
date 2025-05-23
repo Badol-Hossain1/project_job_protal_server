c7b8f49e6e2f4129df554e25e9e9ab4f97a9ae6f
Badol-Hossain1 <badolhossain586@gmail.com>
2025-05-23 21:54:26 +0600

    feat: adding jwt auth

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
 const app = express();
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser())
 
 
 
@@ -14,7 +17,7 @@ app.use(express.json());
 
 
 
-const uri = `mongodb+srv://job_hunter-one:KF2yHFm12zBDIum7@cluster0.l5injar.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB}:${process.env.PASS}@cluster0.l5injar.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -32,9 +35,147 @@ async function run() {
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    //  find all the jobs here 
+     const db = client.db('job_hounter')
+     const jobsCollection = db.collection('jobs') 
+     const applicationCollection = db.collection('applications')
+    app.get('/jobs', async (req, res) => {
+      const email = req.query.email
+      let query = {}
+      if(email){
+        query = {hr_email: email}
+      }
+        const cursor = jobsCollection.find(query)
+        const result = await cursor.toArray()
+       
+
+        res.send(result)
+
+    })
+
+    // find by id 
+    app.get('/jobs/:id', async (req,res)=> {
+      const id =   req.params.id
+      const query = {_id: new ObjectId(id)}
+      const result = await jobsCollection.findOne(query)
+      res.send(result)
+    })
+
+    // view all applicant 
+    app.get('/job-applications/jobs/:job_id',async(req,res) => {
+      const jobId = req.params.job_id
+    
+      const query = {job_id: jobId}
+      const result = await applicationCollection.find(query).toArray()
+      res.send(result)
+    })
+
+    // job application 
+    app.post('/job-applications', async (req,res) => {
+        const application = req.body
+        const result = await applicationCollection.insertOne(application)
+        // const id = application.job_id
+        
+        // const query = {_id: new ObjectId(id)}
+        // const job = await jobsCollection.findOne(query)
+        // const count = 0;
+        // if(job.applicationCount){
+        //   count = job.applicationCount + 1
+        // }
+        // else{
+        //   count = 1
+        // }
+
+        // const filter = {_id: new ObjectId(id)}
+        
+
+         
+
+          
+      // const updateDoc = {
+      //     $set: {
+      //        applicationCount: count
+      //    },
+      // };
+
+      // const updateResult = await jobsCollection.updateOne(filter,updateDoc)
+
+
+        res.send(result)
+    })
+
+    // find all apply job 
+    app.get('/job-application', async (req, res) => {
+        const email = req.query.email
+        const query = {applicant_email: email}
+        const result = await applicationCollection.find(query).toArray()
+        // find jobs details by id 
+        for(const job of result){
+           
+          
+            const query1 = {_id: new ObjectId(job.job_id)}
+            const result1 = await jobsCollection.findOne(query1)
+            if(result1){
+                job.title= result1.title;
+                job.company = result1.company;
+                job.company_logo = result1.company_logo;
+                job.location = result1.location;
+                job.hr_email= result1.hr_email;
+                job.company_logo= result1.company_logo
+            }
+        }
+        res.send(result)
+
+
+    })
+
+    // new jobs added 
+    app.post('/jobs' , async (req,res) => {
+      const data = req.body
+      const result = await jobsCollection.insertOne(data)
+
+    
+      res.send(result)
+    })
+
+    // delete 
+    app.delete('/job-applications/:id',async (req,res)=> {
+      const id = req.params.id
+      const query = {_id: new ObjectId(id)}
+      const result = await applicationCollection.deleteOne(query)
+      res.send(result)
+
+
+
+    })
+
+    // update job status 
+    app.patch('/job-applications/:id' ,async(req,res) => {
+      const id = req.params.id
+      const data = req.body
+      const filter = {_id: new ObjectId(id)}
+      const updateDoc = {
+        $set:{
+          status: data.status
+        }
+      }
+      const result = await jobsCollection.updateOne(filter,updateDoc)
+      res.send(result)
+    })
+
+    // auth 
+    app.post('/jwt',async(req,res)=> {
+      const user = req.body
+      const token = jwt.sign(user,process.env.SECRET,{expiresIn:'1h'})
+      res.send(token)
+
+    })
+
+
+
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
